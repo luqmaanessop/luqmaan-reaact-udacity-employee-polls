593bd3ba45031c13983755245232efea49cab965
Luqmaan Essop <luqmaan.essop@amazee.com>
2023-11-30 08:09:31 +0200

    feat: work on pollpage

--- src/components/PollPage.js
+++ src/components/PollPage.js
@@ -1,6 +1,7 @@
-import { useNavigate } from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
+import {connect} from "react-redux";
 
-const PollPage = () => {
+const PollPage = ({dispatch, authedUser, question, author}) => {
     const navigate = useNavigate();
 
     const handleOptionOne = (e) => {
@@ -20,10 +21,10 @@ const PollPage = () => {
 
     return (
         <div className="max-w-lg mx-auto p-6 bg-white shadow-md rounded-md">
-            <h1 className="text-3xl font-bold mt-9">Poll by AUTHOR ID HERE</h1>
+            <h1 className="text-3xl font-bold mt-9 text-center mb-2">{author.id}'s Poll</h1>
 
             <div className="flex justify-center">
-                <img src={''} alt="Profile" className="h-24 w-24" />
+                <img src={author.avatarURL} alt="Profile" className="h-24 w-24" />
             </div>
 
             <div className="flex justify-center">
@@ -36,7 +37,7 @@ const PollPage = () => {
                     className="p-2 rounded-xl  hover:shadow-xl transition"
                 >
                     <div>
-                        <p className="font-bold mb-2">Option 1</p>
+                        <p className="font-bold mb-2">{question.optionOne.text}</p>
                         <p className="bg-blue-500 text-white px-4 py-2 rounded-md mr-2">Click</p>
                     </div>
                 </button>
@@ -45,7 +46,7 @@ const PollPage = () => {
                     className="p-2 rounded-xl"
                 >
                     <div>
-                        <p className="font-bold mb-2">Option 2</p>
+                        <p className="font-bold mb-2">{question.optionTwo.text}</p>
                         <p className="bg-blue-500 text-white px-4 py-2 rounded-md mr-2">Click</p>
                     </div>
                 </button>
@@ -53,5 +54,10 @@ const PollPage = () => {
         </div>
     );
 };
+const mapStateToProps = ({authedUser, users, questions}) => {
+    const question = Object.values(questions).find((question) => question.id === useParams().id);
+    const author = Object.values(users).find((user) => user.id === question.author);
+    return {authedUser, question, author};
+};
 
-export default PollPage;
+export default connect(mapStateToProps)(PollPage);
